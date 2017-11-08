0def744c0a8c96ffde505d0fdd9956f936ff96d5
Matej Ferenc <matej.ferenc@monster.com>
2017-11-08 17:08:59 +0100

    new results

--- src/main/java/com/mf/blackjack/Blackjack.java
+++ src/main/java/com/mf/blackjack/Blackjack.java
@@ -1,23 +1,19 @@
 package com.mf.blackjack;
 
+import com.mf.evolution.*;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import com.mf.evolution.EvolutionAlgorithm;
-import com.mf.evolution.Individual;
-import com.mf.evolution.Mutator;
-import com.mf.evolution.Population;
-import com.mf.evolution.PopulationSelector;
-
 public class Blackjack {
 
     public static void main(String args[]) {
-        BlackjackPopulationInitializer populationInitializer = new BlackjackPopulationInitializer(10, new BlackjackDnaBuilder());
+        BlackjackPopulationInitializer populationInitializer = new BlackjackPopulationInitializer(100, new BlackjackDnaBuilder());
         PopulationSelector populationSelector = new PopulationSelector(0.3);
         BlackjackDnaBuilder dnaBuilder = new BlackjackDnaBuilder();
         BlackjackCrossoverBuilder blackjackCrossoverBuilder = new BlackjackCrossoverBuilder(dnaBuilder);
-        Mutator mutator = new Mutator(0.1, 5, dnaBuilder);
+        Mutator mutator = new Mutator(0.1, 10, dnaBuilder);
         BlackjackConditionsEvaluator conditionsEvaluator = new BlackjackConditionsEvaluator();
         EvolutionAlgorithm evolutionAlgorithm = new EvolutionAlgorithm(populationSelector, blackjackCrossoverBuilder,
                 mutator, populationInitializer, conditionsEvaluator);
